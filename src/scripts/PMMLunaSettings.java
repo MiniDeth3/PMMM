7c859857f4ce0c2d90ae97f2a67b9a6c3318a37f
TouchOfVanilla <116687689+TouchOfVanilla@users.noreply.github.com>
2024-03-15 12:49:48 -0400

    adjustable weapon glow lunasetting

--- src/scripts/PMMLunaSettings.java
+++ src/scripts/PMMLunaSettings.java
@@ -1,4 +1,17 @@
 package scripts;
 
+import com.fs.starfarer.api.Global;
+import lunalib.lunaSettings.LunaSettings;
+
+import java.awt.*;
+
 public class PMMLunaSettings {
+    public static Color PirateGlowColor(){
+        Color pirateglowcolor = new Color(255,255,255,255);
+        if (Global.getSettings().getModManager().isModEnabled("lunalib"))
+        {
+            pirateglowcolor = LunaSettings.getColor("PirateMiniMegaMod","pmm_piratemodsglow");
+        }
+        return pirateglowcolor;
+    }
 }
