49581489f7d6cf12d9a92a18f51641d4cdd26234
TouchOfVanilla <116687689+TouchOfVanilla@users.noreply.github.com>
2024-04-21 15:07:21 -0400

    Gamma core piraye autos
    -pirate gamma officer constructor
    -25% chance for every auto hull in pirate fleets to get a gamma core

--- src/data/listeners/PirateFleetInflationListener.java
+++ src/data/listeners/PirateFleetInflationListener.java
@@ -5,10 +5,21 @@ import com.fs.starfarer.api.campaign.CampaignFleetAPI;
 import com.fs.starfarer.api.campaign.FactionAPI;
 import com.fs.starfarer.api.campaign.FleetInflater;
 import com.fs.starfarer.api.campaign.listeners.FleetInflationListener;
+import com.fs.starfarer.api.characters.FullName;
+import com.fs.starfarer.api.characters.PersonAPI;
 import com.fs.starfarer.api.fleet.FleetMemberAPI;
+import com.fs.starfarer.api.impl.campaign.AICoreOfficerPluginImpl;
+import com.fs.starfarer.api.impl.campaign.ids.Personalities;
+import com.fs.starfarer.api.impl.campaign.ids.Ranks;
+import com.fs.starfarer.api.impl.campaign.ids.Skills;
 import com.fs.starfarer.api.impl.campaign.ids.Stats;
+import com.fs.starfarer.api.impl.campaign.rulecmd.salvage.AICores;
+import org.lazywizard.lazylib.MathUtils;
 
 import java.util.ArrayList;
+
+import static com.fs.starfarer.api.campaign.AICoreOfficerPlugin.AUTOMATED_POINTS_MULT;
+import static com.fs.starfarer.api.campaign.AICoreOfficerPlugin.AUTOMATED_POINTS_VALUE;
 
 public class PirateFleetInflationListener implements FleetInflationListener {
     public static final String COMPMODS = "compmods"; //target hullmod ID
@@ -31,6 +42,13 @@ public class PirateFleetInflationListener implements FleetInflationListener {
             if(fleetMemberAPI.isStation())continue;
             if(fleetMemberAPI.isMothballed())continue;
             if(!pirate)continue;
+
+            if (fleetMemberAPI.getVariant().hasHullMod("automated")){
+                float prob = MathUtils.getRandomNumberInRange(1,100);
+                if (prob > 75){
+                    fleetMemberAPI.setCaptain(createPirateGammaCoreOfficer());
+                }
+            }
             if(!chance)continue;
 
             int SModsAmount = (determineAmountofSmods(fleet.getFaction()));
@@ -42,6 +60,34 @@ public class PirateFleetInflationListener implements FleetInflationListener {
 
     public int determineAmountofSmods(FactionAPI factionAPI) {
         return getRandomNumber(0,1);
+    }
+
+    public static PersonAPI createPirateGammaCoreOfficer(){
+
+        PersonAPI person = Global.getFactory().createPerson();
+        person.setFaction("pirates");
+        person.setAICoreId("gamma_core");
+
+        person.getStats().setSkipRefresh(true);
+
+        person.setName(new FullName("Gamma", "", FullName.Gender.ANY));
+
+        person.setPortraitSprite("graphics/portraits/portrait_ai1b.png");
+        person.getStats().setLevel(3);
+        person.getStats().setSkillLevel(Skills.HELMSMANSHIP, 2);
+        person.getStats().setSkillLevel(Skills.IMPACT_MITIGATION, 2);
+        //person.getStats().setSkillLevel(Skills.RELIABILITY_ENGINEERING, 2);
+        person.getStats().setSkillLevel(Skills.COMBAT_ENDURANCE, 2);
+
+        person.getMemoryWithoutUpdate().set(AUTOMATED_POINTS_MULT, 2);
+
+        person.setPersonality(Personalities.RECKLESS);
+        person.setRankId(Ranks.SPACE_CAPTAIN);
+        person.setPostId(null);
+
+        person.getStats().setSkipRefresh(false);
+
+        return person;
     }
 
 }
