6b0813c10a2f922516a14caf2ba218390b9d7686
MiniDeth3 <142844055+MiniDeth3@users.noreply.github.com>
2024-05-04 20:13:19 -0500

    Update PirateFleetInflationListener.java

--- src/data/listeners/PirateFleetInflationListener.java
+++ src/data/listeners/PirateFleetInflationListener.java
@@ -8,48 +8,43 @@ import com.fs.starfarer.api.campaign.listeners.FleetInflationListener;
 import com.fs.starfarer.api.characters.FullName;
 import com.fs.starfarer.api.characters.PersonAPI;
 import com.fs.starfarer.api.fleet.FleetMemberAPI;
-import com.fs.starfarer.api.impl.campaign.AICoreOfficerPluginImpl;
 import com.fs.starfarer.api.impl.campaign.ids.Personalities;
 import com.fs.starfarer.api.impl.campaign.ids.Ranks;
 import com.fs.starfarer.api.impl.campaign.ids.Skills;
 import com.fs.starfarer.api.impl.campaign.ids.Stats;
-import com.fs.starfarer.api.impl.campaign.rulecmd.salvage.AICores;
-import org.lazywizard.lazylib.MathUtils;
-
-import java.util.ArrayList;
 
 import static com.fs.starfarer.api.campaign.AICoreOfficerPlugin.AUTOMATED_POINTS_MULT;
-import static com.fs.starfarer.api.campaign.AICoreOfficerPlugin.AUTOMATED_POINTS_VALUE;
 
 public class PirateFleetInflationListener implements FleetInflationListener {
-    public static final String COMPMODS = "pmm_compmods"; //target hullmod ID
-    public static final float CHANCE = 0.30f; //chance for hullmod to spawn smod
+    public static final String COMPMODS = "pmm_compmods"; //target hullmod ID. Oddly this needs to be defined here if using non-vanilla hullmod.
+    public static final float COMPMODS_S_CHANCE = 0.35f; //chance for hullmod to spawn smod
+    public static final float GAMMA_CHANCE = 0.50f; //chance for auto ship to spawn with core
     public int getRandomNumber(int min, int max) {
         return (int) ((Math.random() * (max - min)) + min);
     }
     @Override
     public void reportFleetInflated(CampaignFleetAPI fleet, FleetInflater inflater) {
 
-        if(fleet.isPlayerFleet()) return;
-        if(fleet.getFleetData().getCommander().equals(Global.getSector().getPlayerPerson())) return;
+        if(fleet.isPlayerFleet()) return; //if player fleet no effect
+        if(fleet.getFleetData().getCommander().equals(Global.getSector().getPlayerPerson())) return; //if player in fleet no effect
         if(!fleet.getFaction().getId().contains("pirate")) return; //change the quote to the ID of target faction
 
         for (FleetMemberAPI fleetMemberAPI : fleet.getMembersWithFightersCopy()) {
-            boolean pirate = fleetMemberAPI.getHullSpec().getManufacturer().contains("Pirate"); //change the quote to your target ships manufacturer/tech
-            boolean chance = (Math.random() < CHANCE);
+            boolean pirate = fleetMemberAPI.getHullSpec().getManufacturer().contains("Pirate"); //change the quote to your target ships manufacturer or tech
+            boolean smodchance = (Math.random() < COMPMODS_S_CHANCE); //sets up dice roll for smod chance
+            boolean aicorechance = (Math.random() < GAMMA_CHANCE); //sets up dice roll for ai core chance
 
-            if(fleetMemberAPI.isFighterWing())continue;
-            if(fleetMemberAPI.isStation())continue;
-            if(fleetMemberAPI.isMothballed())continue;
-            if(!pirate)continue;
+            if(fleetMemberAPI.isFighterWing())continue; //if fighter no effect
+            if(fleetMemberAPI.isStation())continue; //if station no effect
+            if(fleetMemberAPI.isMothballed())continue; //if mothballed no effect
+            if(!pirate)continue; //if not specified tech then no effect
 
             if (fleetMemberAPI.getVariant().hasHullMod("automated")){
-                float prob = MathUtils.getRandomNumberInRange(1,100);
-                if (prob > 75){
+                if (aicorechance){
                     fleetMemberAPI.setCaptain(createPirateGammaCoreOfficer());
                 }
             }
-            if(!chance)continue;
+            if(!smodchance)continue; //if dice roll fails then no effect
 
             int SModsAmount = (determineAmountofSmods(fleet.getFaction()));
             fleetMemberAPI.getStats().getDynamic().getMod(Stats.MAX_PERMANENT_HULLMODS_MOD).modifyFlat("Modifications", (SModsAmount));
@@ -82,7 +77,7 @@ public class PirateFleetInflationListener implements FleetInflationListener {
         person.getMemoryWithoutUpdate().set(AUTOMATED_POINTS_MULT, 2);
 
         person.setPersonality(Personalities.RECKLESS);
-        person.setRankId(Ranks.SPACE_CAPTAIN);
+        person.setRankId(Ranks.POST_GANGSTER);
         person.setPostId(null);
 
         person.getStats().setSkipRefresh(false);
