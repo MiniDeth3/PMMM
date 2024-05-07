ec621779b67575fb6b36fdc503673837344cdd7d
TouchOfVanilla <116687689+TouchOfVanilla@users.noreply.github.com>
2024-05-06 21:51:52 -0400

    Update PhaseMineFlakScript.java

--- src/data/weapons/PhaseMineFlakScript.java
+++ src/data/weapons/PhaseMineFlakScript.java
@@ -1,12 +1,15 @@
 package data.weapons;
 
 import com.fs.starfarer.api.Global;
-import com.fs.starfarer.api.combat.CombatEngineAPI;
-import com.fs.starfarer.api.combat.DamagingProjectileAPI;
-import com.fs.starfarer.api.combat.OnFireEffectPlugin;
-import com.fs.starfarer.api.combat.WeaponAPI;
+import com.fs.starfarer.api.combat.*;
+import com.fs.starfarer.api.impl.combat.MineStrikeStats;
+import com.fs.starfarer.api.input.InputEventAPI;
+import com.fs.starfarer.api.util.Misc;
 import org.lazywizard.lazylib.MathUtils;
 import org.lwjgl.util.vector.Vector2f;
+
+import java.awt.*;
+import java.util.List;
 
 public class PhaseMineFlakScript implements OnFireEffectPlugin {
 
@@ -16,11 +19,42 @@ public class PhaseMineFlakScript implements OnFireEffectPlugin {
 
         float minangle = weapon.getArcFacing() - 20;
         float maxangle = weapon.getArcFacing() + 20;
+        float fadeInTime = 0.5F;
 
         Vector2f loc = MathUtils.getRandomPointInCone(weapon.getLocation(), RANGE, minangle, maxangle);
+        Global.getCombatEngine().addPlugin(this.createMissileJitterPlugin((MissileAPI) projectile, fadeInTime));
 
         projectile.getLocation().set(loc);
         Global.getSoundPlayer().playSound("mine_teleport", 1.0F, 1.0F, projectile.getLocation(), projectile.getVelocity());
 
+    }
+
+    protected EveryFrameCombatPlugin createMissileJitterPlugin(final MissileAPI mine, final float fadeInTime) {
+        return new BaseEveryFrameCombatPlugin() {
+            float elapsed = 0.0F;
+
+            public void advance(float amount, List<InputEventAPI> events) {
+                if (!Global.getCombatEngine().isPaused()) {
+                    this.elapsed += amount;
+                    float jitterLevel = mine.getCurrentBaseAlpha();
+                    if (jitterLevel < 0.5F) {
+                        jitterLevel *= 2.0F;
+                    } else {
+                        jitterLevel = (1.0F - jitterLevel) * 2.0F;
+                    }
+
+                    float jitterRange = 1.0F - mine.getCurrentBaseAlpha();
+                    float maxRangeBonus = 50.0F;
+                    float jitterRangeBonus = jitterRange * maxRangeBonus;
+                    Color c = MineStrikeStats.JITTER_UNDER_COLOR;
+                    c = Misc.setAlpha(c, 70);
+                    mine.setJitter(this, c, jitterLevel, 15, jitterRangeBonus * 0.0F, jitterRangeBonus);
+                    if (jitterLevel >= 1.0F || this.elapsed > fadeInTime) {
+                        Global.getCombatEngine().removePlugin(this);
+                    }
+
+                }
+            }
+        };
     }
 }
