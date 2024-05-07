20adad5888c14b3530886cc3c3aa2ff33a141ed1
TouchOfVanilla <116687689+TouchOfVanilla@users.noreply.github.com>
2024-05-06 21:25:25 -0400

    Update PhaseMineFlakScript.java

--- src/data/weapons/PhaseMineFlakScript.java
+++ src/data/weapons/PhaseMineFlakScript.java
@@ -6,11 +6,20 @@ import com.fs.starfarer.api.combat.DamagingProjectileAPI;
 import com.fs.starfarer.api.combat.OnFireEffectPlugin;
 import com.fs.starfarer.api.combat.WeaponAPI;
 import org.lazywizard.lazylib.MathUtils;
+import org.lwjgl.util.vector.Vector2f;
 
 public class PhaseMineFlakScript implements OnFireEffectPlugin {
+
+    public static float RANGE = 600;
     @Override
     public void onFire(DamagingProjectileAPI projectile, WeaponAPI weapon, CombatEngineAPI engine) {
-        projectile.getLocation().set(MathUtils.getRandomPointInCircle(projectile.getSource().getLocation(), projectile.getSource().getCollisionRadius()));
+
+        float minangle = weapon.getArcFacing() - 20;
+        float maxangle = weapon.getArcFacing() + 20;
+
+        Vector2f loc = MathUtils.getRandomPointInCone(weapon.getLocation(), RANGE, minangle, maxangle);
+
+        projectile.getLocation().set(loc);
         Global.getSoundPlayer().playSound("mine_teleport", 1.0F, 1.0F, projectile.getLocation(), projectile.getVelocity());
 
     }
