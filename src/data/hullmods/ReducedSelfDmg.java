e9d6c7897928c7f0aeb97a3cf0198a6f479542f6
TouchOfVanilla <116687689+TouchOfVanilla@users.noreply.github.com>
2024-05-14 19:07:13 -0400

    eeby deeby

--- src/data/hullmods/ReducedSelfDmg.java
+++ src/data/hullmods/ReducedSelfDmg.java
@@ -8,7 +8,7 @@ import org.lwjgl.util.vector.Vector2f;
 public class ReducedSelfDmg implements OnHitEffectPlugin {
     @Override
     public void onHit(DamagingProjectileAPI projectile, CombatEntityAPI target, Vector2f point, boolean shieldHit, ApplyDamageResultAPI damageResult, CombatEngineAPI engine) {
-        if (projectile.getSource() == target){
+        if (target.getOwner() == 0){
             damageResult.setDamageToShields(damageResult.getDamageToShields()/10);
             damageResult.setDamageToHull(damageResult.getDamageToHull()/10);
             damageResult.setTotalDamageToArmor(damageResult.getTotalDamageToArmor()/10);
