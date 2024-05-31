4a28705a09c81a7dd86092f2862055e372c5933f
Aleksei Klyshnniov <alexey.klyshnikov@gmail.com>
2024-05-31 19:17:43 +0400

    Play with reflection a bit more still no luck

--- src/data/hullmods/ReflectivePlate.java
+++ src/data/hullmods/ReflectivePlate.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 
 public class ReflectivePlate extends BaseHullMod {
-    public static final float BEAM_ABSORPTION = 0.0f;
+    public static final float BEAM_ABSORPTION = 0.01f;
     private ReflectionManager reflectionManager;
 
     @Override
@@ -24,8 +24,6 @@ public class ReflectivePlate extends BaseHullMod {
         ship.getMutableStats().getArmorDamageTakenMult().modifyMult("ReflectivePlate", BEAM_ABSORPTION);
         ship.getMutableStats().getHullDamageTakenMult().modifyMult("ReflectivePlate", BEAM_ABSORPTION);
     }
-
-    Map<String, Integer> removeCandidates = new HashMap<>();
 
     static final Color TRANSPARENT = new Color(0,0,0,0);
 
@@ -37,8 +35,9 @@ public class ReflectivePlate extends BaseHullMod {
                 && ship.isAlive()
                 && !engine.isCombatOver()) {
 
-            reflectionManager.drawBorders();
-            reflectionManager.processBeams(engine.getBeams());
+
+            BoundsAPI.SegmentAPI segment = reflectionManager.processBeams(engine.getBeams());
+            reflectionManager.drawBorders(segment);
         } else {
 //            Global.getLogger(ReflectivePlate.class).warn(String.format("Ships in combat %s", engine.getShips().size()));
 //            Global.getLogger(ReflectivePlate.class).warn(String.format("Ship with plate: %s isAlive: %s", ship, ship.isAlive()));
@@ -60,15 +59,19 @@ public class ReflectivePlate extends BaseHullMod {
         Map<BeamAPI, ShipAPI> reflectionDrones = new HashMap<>();
         Map<BeamAPI, Integer> reflectionGeneration = new HashMap<>();
 
+        ShipAPI reflectionNormalDrone;
+        int reflectionNormalDroneGeneration = 0;
+
         public ReflectionManager(CombatEngineAPI combatEngine, ShipAPI ship, Logger logger) {
             this.combatEngine = combatEngine;
             this.ship = ship;
             this.logger = logger;
             this.settings = Global.getSettings();
+            this.reflectionNormalDrone = createReflectDrone("lrpdlaser");
             logger.warn(String.format("ReflectionManager::init for %s", ship.getName()));
         }
 
-        void drawBorders(){
+        void drawBorders(BoundsAPI.SegmentAPI givenSegment){
             logger.warn(String.format("Updated ship location: %s", ship.getLocation()));
             logger.warn(String.format("updateBorderDrones segment count == %s", ship.getExactBounds().getSegments().size()));
 
@@ -79,7 +82,10 @@ public class ReflectivePlate extends BaseHullMod {
                     borderDrone = createBorderDrone();
                     borderDrones.put(segmentIdx, borderDrone);
                 }
-
+                if(givenSegment != null
+                        && (segment == givenSegment || segment.getP1().equals(givenSegment.getP2()))) {
+                    borderDrone.setOverloadColor(Color.black);
+                }
                 borderDrone.getLocation().set(segment.getP1());
                 logger.warn(String.format("Updated drone for point %s", segment.getP1()));
 
@@ -90,7 +96,7 @@ public class ReflectivePlate extends BaseHullMod {
         private ShipAPI createBorderDrone () {
             SettingsAPI settings = Global.getSettings();
 
-            ShipVariantAPI dotDroneVariant = Global.getSettings().createEmptyVariant("wasp_wing", settings.getHullSpec("wasp"));
+            ShipVariantAPI dotDroneVariant = Global.getSettings().createEmptyVariant("pmm_dot_green_drone", settings.getHullSpec("pmm_dot_green_drone"));
             ShipAPI dotDrone = combatEngine.createFXDrone(dotDroneVariant);
 
             dotDrone.setLayer(CombatEngineLayers.ABOVE_SHIPS_AND_MISSILES_LAYER);
@@ -101,14 +107,17 @@ public class ReflectivePlate extends BaseHullMod {
             return dotDrone;
         }
 
-        void processBeams(Collection<BeamAPI> beams) {
+        BoundsAPI.SegmentAPI processBeams(Collection<BeamAPI> beams) {
             currentGeneration = (currentGeneration + 1) % GENERATION_COUNT;
+            BoundsAPI.SegmentAPI segment = null;
             for (BeamAPI beam : beams) {
-                if(reflect(beam)) {
+                segment = reflect(beam);
+                if(segment != null) {
                     logger.warn(String.format("Beam [%s, %s] was reflected", beam.getFrom(), beam.getTo()));
                 }
             }
             treatCaches();
+            return segment;
         }
 
         void treatCaches() {
@@ -128,23 +137,24 @@ public class ReflectivePlate extends BaseHullMod {
             }
         }
 
-        boolean reflect(BeamAPI beam) {
+        BoundsAPI.SegmentAPI reflect(BeamAPI beam) {
             if(beam.getDamageTarget() != ship && beam.getDamageTarget() != ship.getParentStation()) {
                 logger.error(String.format("Ship %s is not target %s", ship.getParentStation(), beam.getDamageTarget()));
-                return false;
+                return null;
             }
 
             float reflectedBeamRange = beam.getWeapon().getRange() - beam.getLength();
             if(reflectedBeamRange < 100f) {
                 logger.error(String.format("reflectedBeamRange %s is too short", reflectedBeamRange));
-                return false;
+                return null;
             }
 
-            Vector2f reflectionDirection = getReflectDirection(beam, ship);
+            BoundsAPI.SegmentAPI segment = getIntersectedSegment(beam, ship);
+            Vector2f reflectionDirection = getReflectDirection(beam, segment);
 
             if(reflectionDirection == null) {
                 logger.error("ReflectionManager::reflect reflectionDirection is null for some reson");
-                return false;
+                return null;
             }
 
             logger.error(String.format("Reflecting beam reflectionDirection is %s", reflectionDirection));
@@ -153,11 +163,14 @@ public class ReflectivePlate extends BaseHullMod {
 
             float weaponDisplacement = reflectionDrone.getWeaponGroupsCopy().get(0).getWeaponsCopy().get(0).getSprite().getHeight() / 2f;
 
-            reflectionDrone.getLocation().set(
-                    beam.getTo().x - weaponDisplacement * beam.getTo().x / reflectedBeamRange,
-                    beam.getTo().y - weaponDisplacement * beam.getTo().y / reflectedBeamRange);
+            reflectionNormalDrone.getLocation().set(beam.getTo());
+
+            reflectionNormalDrone.giveCommand(ShipCommand.FIRE, segment.getP1(), 0);
+
+            reflectionDrone.getLocation().set(beam.getTo());
 
             Vector2f target = new Vector2f(reflectionDrone.getLocation().x + reflectionDirection.x, reflectionDrone.getLocation().y + reflectionDirection.y);
+
 
             reflectionDrone.giveCommand(ShipCommand.FIRE, target, 0);
 
@@ -170,40 +183,13 @@ public class ReflectivePlate extends BaseHullMod {
 
             reflectionGeneration.put(beam, currentGeneration);
 
-            return true;
+            return segment;
         }
 
         private ShipAPI getReflectionDrone(BeamAPI beam) {
 
             if(!reflectionDrones.containsKey(beam)) {
-                ShipVariantAPI beamDroneVariant = settings.createEmptyVariant("pmm_beam_drone", settings.getHullSpec("pmm_beam_drone"));
-
-                // Setting up weapon
-                WeaponGroupSpec weaponGroupSpec = new WeaponGroupSpec(WeaponGroupType.LINKED);
-                weaponGroupSpec.addSlot("WS 001");
-                beamDroneVariant.addWeapon("WS 001", beam.getWeapon().getId());
-                beamDroneVariant.addWeaponGroup(weaponGroupSpec);
-
-                ShipAPI beamDrone = combatEngine.createFXDrone(beamDroneVariant);
-                beamDrone.setLayer(CombatEngineLayers.FIGHTERS_LAYER);
-                beamDrone.setCollisionClass(CollisionClass.NONE);
-                beamDrone.setOwner(ship.getOriginalOwner());
-
-                //Apply stats
-                beamDrone.getMutableStats().getArmorDamageTakenMult().modifyMult("Reflective Plate", 0f);
-                beamDrone.getMutableStats().getHullDamageTakenMult().modifyMult("Reflective Plate", 0f);
-                beamDrone.getMutableStats().getShieldDamageTakenMult().modifyMult("Reflective Plate", 0f);
-
-                beamDrone.getMutableStats().getBeamWeaponDamageMult().modifyMult("Reflective Plate", 1f - BEAM_ABSORPTION);
-                beamDrone.getMutableStats().getBeamWeaponFluxCostMult().modifyMult("Reflective Plate", 0);
-
-    //            WeaponAPI beamDroneWeapon = beamDrone.getWeaponGroupsCopy().get(0).getWeaponsCopy().get(0);
-
-    //            beamDroneWeapon.getSprite().setColor(TRANSPARENT);
-    //            beamDroneWeapon.getGlowSpriteAPI().setColor(TRANSPARENT);
-
-                combatEngine.addEntity(beamDrone);
-
+                ShipAPI beamDrone = createReflectDrone(beam.getWeapon().getId());
                 reflectionDrones.put(beam, beamDrone);
                 logger.warn("getReflectionDrone: creating drone");
             } else {
@@ -213,8 +199,39 @@ public class ReflectivePlate extends BaseHullMod {
             return reflectionDrones.get(beam);
         }
 
-        public Vector2f getReflectDirection(BeamAPI beam, ShipAPI ship) { // direction vector
-            BoundsAPI.SegmentAPI segment = getIntersectedSegment(beam, ship);
+        ShipAPI createReflectDrone(String weaponId) {
+            ShipVariantAPI beamDroneVariant = settings.createEmptyVariant("pmm_beam_drone", settings.getHullSpec("pmm_beam_drone"));
+
+            // Setting up weapon
+            WeaponGroupSpec weaponGroupSpec = new WeaponGroupSpec(WeaponGroupType.LINKED);
+            weaponGroupSpec.addSlot("WS 001");
+            beamDroneVariant.addWeapon("WS 001", weaponId);
+            beamDroneVariant.addWeaponGroup(weaponGroupSpec);
+
+            ShipAPI beamDrone = combatEngine.createFXDrone(beamDroneVariant);
+            beamDrone.setLayer(CombatEngineLayers.FIGHTERS_LAYER);
+            beamDrone.setCollisionClass(CollisionClass.NONE);
+            beamDrone.setOwner(ship.getOriginalOwner());
+
+            //Apply stats
+            beamDrone.getMutableStats().getArmorDamageTakenMult().modifyMult("Reflective Plate", 0f);
+            beamDrone.getMutableStats().getHullDamageTakenMult().modifyMult("Reflective Plate", 0f);
+            beamDrone.getMutableStats().getShieldDamageTakenMult().modifyMult("Reflective Plate", 0f);
+
+            beamDrone.getMutableStats().getBeamWeaponDamageMult().modifyMult("Reflective Plate", 1f - BEAM_ABSORPTION);
+            beamDrone.getMutableStats().getBeamWeaponFluxCostMult().modifyMult("Reflective Plate", 0);
+
+            WeaponAPI beamDroneWeapon = beamDrone.getWeaponGroupsCopy().get(0).getWeaponsCopy().get(0);
+
+            beamDroneWeapon.getSprite().setColor(TRANSPARENT);
+            beamDroneWeapon.getGlowSpriteAPI().setColor(TRANSPARENT);
+
+            combatEngine.addEntity(beamDrone);
+
+            return beamDrone;
+        }
+        public Vector2f getReflectDirection(BeamAPI beam, BoundsAPI.SegmentAPI segment) { // direction vector
+
             if (segment != null) {
                 Vector2f beamOrigin = beam.getFrom();
                 Vector2f hitLocation = beam.getTo();
@@ -235,10 +252,10 @@ public class ReflectivePlate extends BaseHullMod {
 
                 float perpendicularProjectionOfBeamOriginY = (standardizedSegmentPointY / standardizedSegmentPointX) * perpendicularProjectionOfBeamOriginX;
 
-                float reflectedX = standardizedBeamOriginX - 2 * perpendicularProjectionOfBeamOriginX;
-                float reflectedY = standardizedBeamOriginY - 2 * perpendicularProjectionOfBeamOriginY;
+//                float reflectedX = standardizedBeamOriginX - 2 * perpendicularProjectionOfBeamOriginX;
+//                float reflectedY = standardizedBeamOriginY - 2 * perpendicularProjectionOfBeamOriginY;
 
-                return new Vector2f(reflectedX, reflectedY);
+                return new Vector2f(perpendicularProjectionOfBeamOriginX, perpendicularProjectionOfBeamOriginY);
             }
 
             logger.error("ReflectionManager::getReflectDirection couldn't find a segment to reflect");
@@ -246,45 +263,40 @@ public class ReflectivePlate extends BaseHullMod {
         }
 
         private BoundsAPI.SegmentAPI getIntersectedSegment(BeamAPI beam, ShipAPI ship) {
+            BoundsAPI.SegmentAPI bestCandidate = null;
+            float bestCandidateDistance = 10f;
+
             for (BoundsAPI.SegmentAPI segment : ship.getExactBounds().getSegments()) {
-                if (isPointOnTheLine(segment.getP1(), segment.getP2(), beam.getTo())) {
-                    return segment;
+                float distanceToTheSegmentSquared = calcSquaredDistanceToTheSegment(segment.getP1(), segment.getP2(), beam.getTo());
+                if(distanceToTheSegmentSquared < bestCandidateDistance){
+                    bestCandidateDistance = distanceToTheSegmentSquared;
+                    bestCandidate = segment;
                 }
             }
             logger.error("ReflectionManager::getIntersectedSegment couldn't find a segment to reflect");
-            return null;
+            return bestCandidate;
         }
 
-        public boolean isPointOnTheLine(Vector2f lineStart, Vector2f lineEnd, Vector2f point) {
-            // Adjust vectors relative to x1,y1
+        public float calcSquaredDistanceToTheSegment(Vector2f lineStart, Vector2f lineEnd, Vector2f point) {
+            float px = lineEnd.x - lineStart.x;
+            float py = lineEnd.y - lineStart.y;
+            float norm = px * px + py * py;
 
-            logger.warn(String.format("Check if segment [%s, %s] contains point : %s", lineStart, lineEnd, point));
+            float u =  ((point.x - lineStart.y) * px + (point.y - lineStart.y) * py) / norm;
 
-            float squaredDistanceToLineStart = (point.x - lineStart.x) * (point.x - lineStart.x)
-                    + (point.y - lineStart.y) * (point.y - lineStart.y);
-
-            if(squaredDistanceToLineStart < 10f) {
-                logger.warn("true");
-                return true;
+            if (u > 1) {
+                u = 1;
+            } else if (u < 0) {
+                u = 0;
             }
 
-            float squaredDistanceToLineEnd = (point.x - lineEnd.x) * (point.x - lineEnd.x)
-                    + (point.y - lineEnd.y) * (point.y - lineEnd.y);
+            float xClosest = lineStart.x + u * px;
+            float yClosest = lineStart.y + u * py;
 
-            if(squaredDistanceToLineEnd < 10f) {
-                logger.warn("true");
-                return true;
-            }
+            float dx = point.x - xClosest;
+            float dy = point.y - yClosest;
 
-            float squaredLineLength = (lineEnd.x - lineStart.x) * (lineEnd.x - lineStart.x)
-                    + (lineEnd.y - lineStart.y) * (lineEnd.y - lineStart.y);
-
-            boolean result = Math.sqrt(squaredDistanceToLineStart)
-                    + Math.sqrt(squaredDistanceToLineEnd)
-                    - Math.sqrt(squaredLineLength) < squaredLineLength / 1000.0;
-            logger.warn(result);
-
-            return result;
+            return dx * dx + dy * dy;
         }
 
         void cleanup(){
