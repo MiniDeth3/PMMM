ccd76c6376cf60a65b0604b4951e3a358d8d7dda
TouchOfVanilla <116687689+TouchOfVanilla@users.noreply.github.com>
2024-09-18 19:08:44 -0400

    Update ReducedSelfDmg.java

--- src/data/hullmods/ReducedSelfDmg.java
+++ src/data/hullmods/ReducedSelfDmg.java
@@ -5,13 +5,23 @@ import com.fs.starfarer.api.combat.listeners.ApplyDamageResultAPI;
 import com.fs.starfarer.api.combat.listeners.DamageListener;
 import org.lwjgl.util.vector.Vector2f;
 
+import java.awt.*;
+
 public class ReducedSelfDmg implements OnHitEffectPlugin {
     @Override
     public void onHit(DamagingProjectileAPI projectile, CombatEntityAPI target, Vector2f point, boolean shieldHit, ApplyDamageResultAPI damageResult, CombatEngineAPI engine) {
+<<<<<<< Updated upstream
         if (projectile.getSource() == target){
+=======
+        target.getShield().setInnerColor(Color.GREEN);
+
+
+        if (target.getOwner() == 0){
+>>>>>>> Stashed changes
             damageResult.setDamageToShields(damageResult.getDamageToShields()/10);
             damageResult.setDamageToHull(damageResult.getDamageToHull()/10);
             damageResult.setTotalDamageToArmor(damageResult.getTotalDamageToArmor()/10);
+            target.getShield().setInnerColor(Color.PINK);
         }
     }
 }
