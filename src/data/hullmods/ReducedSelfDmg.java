f528ef2b13f8095b027281e06c746b300b62008a
MiniDeth3 <142844055+MiniDeth3@users.noreply.github.com>
2024-09-18 18:12:32 -0500

    ssss

--- src/data/hullmods/ReducedSelfDmg.java
+++ src/data/hullmods/ReducedSelfDmg.java
@@ -5,13 +5,17 @@ import com.fs.starfarer.api.combat.listeners.ApplyDamageResultAPI;
 import com.fs.starfarer.api.combat.listeners.DamageListener;
 import org.lwjgl.util.vector.Vector2f;
 
+import java.awt.*;
+
 public class ReducedSelfDmg implements OnHitEffectPlugin {
     @Override
     public void onHit(DamagingProjectileAPI projectile, CombatEntityAPI target, Vector2f point, boolean shieldHit, ApplyDamageResultAPI damageResult, CombatEngineAPI engine) {
-        if (projectile.getSource() == target){
-            damageResult.setDamageToShields(damageResult.getDamageToShields()/10);
-            damageResult.setDamageToHull(damageResult.getDamageToHull()/10);
-            damageResult.setTotalDamageToArmor(damageResult.getTotalDamageToArmor()/10);
+        target.getShield().setInnerColor(Color.GREEN);
+        if (target.getOwner() == 0){
+            damageResult.setDamageToShields(damageResult.getDamageToShields()/100);
+            damageResult.setDamageToHull(damageResult.getDamageToHull()/100);
+            damageResult.setTotalDamageToArmor(damageResult.getTotalDamageToArmor()/100);
+            target.getShield().setInnerColor(Color.PINK);
         }
     }
 }
