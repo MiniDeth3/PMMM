4e560309fafc48e87b33364709d4dc6e5b2fec5f
MiniDeth3 <142844055+MiniDeth3@users.noreply.github.com>
2024-11-25 21:38:36 -0600

    Update PMMCrossmodScript.java

--- src/scripts/PMMCrossmodScript.java
+++ src/scripts/PMMCrossmodScript.java
@@ -5,49 +5,47 @@ import com.fs.starfarer.api.impl.campaign.ids.Factions;
 
 public class PMMCrossmodScript {
 
-    //Mods
+    // Mods
     public static boolean IsVRIEnabled = false;
     public static boolean IsScrapyardEnabled = false;
     public static boolean IsIndiesEnabled = false;
     public static boolean IsSWPEnabled = false;
 
-    //Ships
+    // Ships
     public static String PIRATE_GAUNTLET = "pmm_gauntlet_p";
     public static String PIRATE_SUPERCHAMP = "pmm_super_champion_p";
     public static String PIRATE_CAVERN = "pmm_cavern_p";
     public static String PIRATE_BRAWLER_TT = "pmm_cavern_p";
-
     public static String PIRATE_STRIKER = "pmm_striker_p";
 
-    //Tags
+    // Tags
     public static String SHIP_TAG_PIRATE_BP = "pirate_bp";
-    public static String SHIP_TAG_PIRATE = "pirates"; //Use addknownships instead
+    public static String SHIP_TAG_PIRATE = "pirates"; // Use addKnownShips instead
     public static String SHIP_TAG_RARE_BP = "rare_bp";
 
-    public static void initVRICrossmod(){
+    public static void initVRICrossmod() {
         IsVRIEnabled = true;
         Global.getSettings().getHullSpec(PIRATE_GAUNTLET).addTag(SHIP_TAG_PIRATE_BP);
         Global.getSector().getFaction(Factions.PIRATES).addKnownShip(PIRATE_GAUNTLET, false);
-
     }
-    public static void initIndiesCrossmod(){
+
+    public static void initIndiesCrossmod() {
         IsIndiesEnabled = true;
         Global.getSettings().getHullSpec(PIRATE_SUPERCHAMP).addTag(SHIP_TAG_PIRATE_BP);
         Global.getSector().getFaction(Factions.PIRATES).addKnownShip(PIRATE_SUPERCHAMP, false);
-
     }
-    public static void initScrapyardCrossmod(){
+
+    public static void initScrapyardCrossmod() {
         IsScrapyardEnabled = true;
         Global.getSettings().getHullSpec(PIRATE_CAVERN).addTag(SHIP_TAG_PIRATE);
         Global.getSettings().getHullSpec(PIRATE_CAVERN).addTag(SHIP_TAG_RARE_BP);
         Global.getSector().getFaction(Factions.PIRATES).addKnownShip(PIRATE_CAVERN, false);
-
     }
-    public static void initSWPCrossmod(){
+
+    public static void initSWPCrossmod() {
         IsSWPEnabled = true;
         Global.getSettings().getHullSpec(PIRATE_STRIKER).addTag(SHIP_TAG_PIRATE);
         Global.getSettings().getHullSpec(PIRATE_STRIKER).addTag(SHIP_TAG_PIRATE_BP);
         Global.getSector().getFaction(Factions.PIRATES).addKnownShip(PIRATE_STRIKER, false);
-
     }
 }
