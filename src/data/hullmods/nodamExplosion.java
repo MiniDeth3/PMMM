c0f3d0fdfc337412256c74b5ec18d57d19358397
MiniDeth3 <142844055+MiniDeth3@users.noreply.github.com>
2024-11-26 19:48:48 -0600

    Update nodamExplosion.java

--- src/data/hullmods/nodamExplosion.java
+++ src/data/hullmods/nodamExplosion.java
@@ -9,15 +9,15 @@ public class nodamExplosion extends BaseHullMod {
 
 	public static final float RADIUS_MULT = 2f;
 	public static final float DAMAGE_MULT = 0.01f;
-	
+
+	@Override
 	public void applyEffectsBeforeShipCreation(HullSize hullSize, MutableShipStatsAPI stats, String id) {
 		stats.getDynamic().getStat(Stats.EXPLOSION_DAMAGE_MULT).modifyMult(id, DAMAGE_MULT);
 		stats.getDynamic().getStat(Stats.EXPLOSION_RADIUS_MULT).modifyMult(id, RADIUS_MULT);
 	}
-	
+
+	@Override
 	public String getDescriptionParam(int index, HullSize hullSize) {
-		return null;
+		return null; // No description parameters defined
 	}
-
-
 }
