c950e729f02d2028cfa3e36ea461c7759c906094
MiniDeth3 <142844055+MiniDeth3@users.noreply.github.com>
2024-11-28 16:51:34 -0600

    Update PMM_c2flux.java

--- src/data/hullmods/PMM_c2flux.java
+++ src/data/hullmods/PMM_c2flux.java
@@ -1,14 +1,14 @@
 package data.hullmods;
 
-import com.fs.starfarer.api.Global;
-import com.fs.starfarer.api.combat.*;
+import com.fs.starfarer.api.combat.BaseHullMod;
+import com.fs.starfarer.api.combat.MutableShipStatsAPI;
+import com.fs.starfarer.api.combat.ShipAPI;
 import com.fs.starfarer.api.combat.ShipAPI.HullSize;
 
-public class PMM_c2flux extends BaseHullMod {
+public class PMM_C2Flux extends BaseHullMod {
 
 	@Override
 	public void advanceInCombat(ShipAPI ship, float amount) {
-		CombatEngineAPI engine = Global.getCombatEngine();
 		// Ensure flux level is below 70% before applying the flux set
 		if (ship.getFluxTracker().getFluxLevel() < 0.7f) {
 			ship.getFluxTracker().setCurrFlux(9100f); // Set the flux to a specific value (adjust as needed)
